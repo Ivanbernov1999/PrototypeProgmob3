24f45fde1c670aaec898197b16c884fa2a72f4e1
ivanbernov1999 <ivanbernov76@gmail.com>
2019-11-26 22:29:02 +0700

    tugas

---
+++ app/src/main/java/prototypeprogmobtgs/com/MainActivity.java
@@ -0,0 +1,20 @@
+package prototypeprogmobtgs.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
